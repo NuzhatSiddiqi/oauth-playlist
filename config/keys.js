9dfaa8fd36d119ab123f6f869cefaa2869d6f30d
Nuzhat Siddiqi <46729206+NuzhatSiddiqi@users.noreply.github.com>
2019-08-31 22:09:49 -0400

    project updating

--- config/keys.js
+++ config/keys.js
@@ -5,5 +5,11 @@ module.exports = {
     clientID:
       "877810967320-t5ru3pvh4rg6ucmjvq5m4j7tutfsplah.apps.googleusercontent.com",
     clientSecret: "KTa5Am14PJZoHZQVYhHvMnCK"
+  },
+  mongodb: {
+    dbURI: "mongodb://Asad:Hassan12>@ds157857.mlab.com:57857/heroku_98n1367p"
+  },
+  session: {
+    cookieKey: "weplaner is the best "
   }
 };
