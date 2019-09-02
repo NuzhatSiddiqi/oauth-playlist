f791d54ca581bf6450c385bf5017aa472bd1b40c
Nuzhat Siddiqi <46729206+NuzhatSiddiqi@users.noreply.github.com>
2019-09-02 13:34:15 -0400

    final project updates

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
 const authRoutes = require("./routes/auth-routes");
+const profileRoutes = require("./routes/profile-routes");
 const passportSetup = require("./config/passport-setup");
 const mongoose = require("mongoose");
 const keys = require("./config/keys");
@@ -30,10 +31,11 @@ mongoose.connect(keys.mongodb.dbURI, () => {
 
 // set up routes
 app.use("/auth", authRoutes);
+app.use("/profile", profileRoutes);
 
 // create home route
 app.get("/", (req, res) => {
-  res.render("home");
+  res.render("home", { user: req.user });
 });
 
 app.listen(3000, () => {
